e014bd4abe8a704fcee69f98e7dad4c936def5b6
niehusst <liamniehusstaab@gmail.com>
2023-12-01 20:14:44 -0800

    add helper to init rust projects

---
+++ getDay.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#takes day int of the day's input data etc to fetch
+# https://www.reddit.com/r/adventofcode/comments/a2vonl/how_to_download_inputs_with_a_script/
+
+if [[ -z "$1" ]]
+then
+    echo "please provide the day"
+    exit 1
+fi
+
+cargo new day$1
+curl "https://adventofcode.com/2022/day/$1/input" \
+  --cookie "session=SESSION" \
+  --compressed > day$1/src/input.txt
