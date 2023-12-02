6713390945dad3169d91fe46b95acb0f95a53e22
niehusst <liamniehusstaab@gmail.com>
2023-12-01 22:12:38 -0800

    Revert "complete day 1"
    This reverts commit 48e6ce82e34e52638e91b389a5088345ebc0df8e.

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/home/niehusst/Desktop/projects/advent2023/day1/target/debug/day1: /home/niehusst/Desktop/projects/advent2023/day1/src/main.rs
--- day1/target/debug/deps/day1-1c601c97a7a57879.d
+++
@@ -1,5 +0,0 @@
-/home/niehusst/Desktop/projects/advent2023/day1/target/debug/deps/day1-1c601c97a7a57879: src/main.rs
-
-/home/niehusst/Desktop/projects/advent2023/day1/target/debug/deps/day1-1c601c97a7a57879.d: src/main.rs
-
-src/main.rs:
